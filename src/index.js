a29d3f3640a1d24953a9d2c22ef5c665d6ebfce1
Calvin Liang <145249380+LiangCalvin@users.noreply.github.com>
2024-03-01 11:21:40 +0700

    first commit

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+
+    <h1>hello</h1>
     <App />
   </React.StrictMode>
 );
